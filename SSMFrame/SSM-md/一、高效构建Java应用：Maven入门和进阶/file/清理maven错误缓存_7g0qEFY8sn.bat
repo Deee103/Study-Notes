e0f77e0001d6cf7597b2972ef89ff86d04661724
Deee103 <1711853899@qq.com>
2024-05-12 18:02:26 +0800

    update

--- SSMFrame/SSM-md/一、高效构建Java应用：Maven入门和进阶/file/清理maven错误缓存_7g0qEFY8sn.bat
+++ SSMFrame/SSM-md/一、高效构建Java应用：Maven入门和进阶/file/清理maven错误缓存_7g0qEFY8sn.bat
@@ -1,6 +1,6 @@
 @echo off
 rem 这里写你的仓库路径
-set REPOSITORY_PATH=D:\repository
+set REPOSITORY_PATH=D:\Program Files\resource\Maven\apache-maven-3.9.6\repository
 rem 正在搜索...
 for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
     del /s /q %%i
